2c0e032d28547062d7a2842884a1b0d4f52709a1
wujixialan <1571449371@qq.com>
2017-06-10 11:00:13 +0800

    更新 2017/6/10 10:58

---
+++ src/main/java/TestGenerator.java
@@ -0,0 +1,40 @@
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by zxg on 2017/6/10.
+ */
+public class TestGenerator {
+    public static void main(String[] args) {
+        List<String> warnings = new ArrayList<>();
+        boolean overwrite = true;
+        File configFile = new File("mbg.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        try {
+            Configuration config = cp.parseConfiguration(configFile);
+            DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+            MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+            myBatisGenerator.generate(null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (XMLParserException e) {
+            e.printStackTrace();
+        } catch (InvalidConfigurationException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/zxg/ssmcurd/beans/Employee.java
@@ -0,0 +1,86 @@
+package com.zxg.ssmcurd.beans;
+
+public class Employee {
+    private Integer empId;
+
+    private String empName;
+
+    private String empEmail;
+
+    private String empGender;
+
+    private Integer dId;
+
+    private Department dept;
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "empId=" + empId +
+                ", empName='" + empName + '\'' +
+                ", empEmail='" + empEmail + '\'' +
+                ", empGender='" + empGender + '\'' +
+                ", dept=" + dept +
+                '}';
+    }
+
+    public Department getDept() {
+        return dept;
+    }
+
+    public void setDept(Department dept) {
+        this.dept = dept;
+    }
+
+    public Integer getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(Integer empId) {
+        this.empId = empId;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName == null ? null : empName.trim();
+    }
+
+    public String getEmpEmail() {
+        return empEmail;
+    }
+
+    public void setEmpEmail(String empEmail) {
+        this.empEmail = empEmail == null ? null : empEmail.trim();
+    }
+
+    public String getEmpGender() {
+        return empGender;
+    }
+
+    public void setEmpGender(String empGender) {
+        this.empGender = empGender == null ? null : empGender.trim();
+    }
+
+    public Integer getdId() {
+        return dId;
+    }
+
+    public void setdId(Integer dId) {
+        this.dId = dId;
+    }
+
+    public Employee() {
+    }
+
+    public Employee(Integer empId, String empName, String empEmail, String empGender, Integer dId) {
+        this.empId = empId;
+        this.empName = empName;
+        this.empEmail = empEmail;
+        this.empGender = empGender;
+        this.dId = dId;
+    }
+
+}
